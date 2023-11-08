3583421004c08157a9fd5c42e1323dce0e4c2417
nhat0120230 <nhatnguyen221020032@gmail.com>
2023-11-08 16:32:30 +0700

    bài tập

---
+++ js.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập vào chiều dài");
+document.write("The Width is : " + inputWidth);
+document.write('<br>');
+inputHeight = prompt("Nhập vào chiều rộng");
+document.write("The Height is : " + inputHeight);
+document.write('<br>');
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let aria = inputWidth * inputHeight;
+document.write("The aria is : " + aria);
